c5077210b2edeb4ddacafbf105ffc27eac815d82
Pappol <riccardo.parola@studenti.unitn.it>
2020-05-05 16:45:02 +0200

    Q process incorporated

--- src/counter/clib.c
+++ src/counter/clib.c
@@ -22,4 +22,106 @@ int writePipe(int pipe[],string msg){
     return ret;
 }
 
+//---------------------------ric functions----
+
+int readFile(char* filename, char* filedata, int start, int stop){
+    int sk, rd;
+    int file = open(filename, O_RDONLY);
+    if(file>=0){
+        sk = lseek(file, start*sizeof(char), SEEK_SET);
+        if(sk != -1){
+            rd = read(file, filedata, stop-start);
+        }
+    }
+
+    lseek(file, 0, SEEK_END);
+    if(file>=0 && sk>=0 && rd>=0) return 0;
+    else return -1;
+}
+
+void countLetters(int dim, char* s, int* counter){
+    //printf("testo cont: ");
+    int i = dim-1;
+    for(i; i>=0; i--){
+        
+        //printf("%c\t", s[i]);
+        if(s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z'){
+            //lettera maiuscola o minuscola
+            counter[0]++;
+        }else{
+            if(s[i]>='0' && s[i]<='9'){
+                //numero
+                counter[1]++;
+            }else{
+                if(s[i]==' '){
+                    //spazi
+                    counter[2]++;
+                }else{
+                    if(s[i]=='.' || s[i]==',' || s[i]==':' || s[i]==';' || s[i]=='?' || s[i]=='!'){
+                        //punteggiatura
+                        counter[3]++;
+                    }else{
+                        //tutto il resto
+                        counter[4]++;
+                    }
+                }
+            }      
+        }
+    }
+}
+
+int* processoQ(int from, int to, char* fname){
+    char* testo;
+    int* stats;
+    int i, 
+        inizio = from, 
+        fine = to;
+    int size;
+
+    testo = malloc(fine*sizeof(char));
+    stats = malloc(5*sizeof(int));
+    i = 0;
+
+    for(i; i<5; i++){
+        stats[i] = 0;
+    }
+
+    i = readFile(fname, testo, inizio, fine);
+
+    countLetters(size-inizio, testo, stats);
+
+    if(i==0)
+        return stats;
+    else 
+        return (int *)-1;
+}
+
+int* processoQ_n(int from, int to, char** fname, int n){
+    char* testo;
+    int* stats;
+    int i,j, 
+        inizio = from, 
+        fine = to;
+
+    testo = malloc(fine*sizeof(char));
+    stats = malloc(5*sizeof(int));
+    i = 0;
+
+    for(i; i<5; i++){
+        stats[i] = 0;
+    }
+
+    for(j=0; j<n; j++){      
+        i = readFile(fname[j], testo, inizio, fine);
+        countLetters(fine-inizio, testo, stats);
+        if(i<0) break;
+    }
+
+
+    if(i==0)
+        return stats;
+    else 
+        return (int *)-1;
+}
+
 #endif
--- src/counter/counter.h
+++ src/counter/counter.h
@@ -5,12 +5,28 @@
 #include <sys/shm.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <unistd.h>
+
 
 #define READ 0
 #define WRITE 1
 #define MAXLEN 100
+#define BOOL char
+#define FALSE 0
+#define TRUE 1
+#define BUFFSIZE 1000
 
 typedef char * string;
 
 int writePipe(int pipe[],string msg);
 string readAndWait(int pipe[], pid_t son);
+
+//funzioni di ric
+
+int readFile(char* filename, char* filedata, int start, int stop);
+
+void countLetters(int dim, char* s, int* counter);
+
+int* processoQ(int from, int to, char* fname);
+
+int* processoQ_n(int from, int to, char** fname, int n);
