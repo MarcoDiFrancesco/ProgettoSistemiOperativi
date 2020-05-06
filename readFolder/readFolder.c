8f10a4153a76c11b2e30ae19ed656032c3477330
MarcoDiFrancesco <marcodifran@gmail.com>
2020-05-06 22:52:58 +0200

    ReadFolder add, update readFolder location

--- readFolder/readFolder.c
+++ readFolder/readFolder.c
@@ -1,34 +1,94 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/file.h>
-#include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
-int main(){
-	char* result;
-	int i;
-	
-	
-	
-	system("cd ./folder && ls >> elenco.txt");
-	system("cd ./folder && mv elenco.txt ..");
-	
+// https://www.hackerearth.com/practice/data-structures/linked-list/singly-linked-list/tutorial/
+struct LinkedList {
+    char *str;
+    struct LinkedList *next;
+};
 
-	int file = open("elenco.txt", O_RDONLY);	
-	
-	int dim = lseek(file, 0, SEEK_END);
-	result = malloc(dim);
-	lseek(file, 0, SEEK_SET);
-	
-	if(read(file, result, dim)<0){
-		printf("--->cazzi");	
-	}
-	
-	
-	for(i=0; i<dim; i++){
-		printf("%c", result[i]);
-	}
+typedef struct LinkedList *node;
 
-	return 0;
+node createNode() {
+    node temp;
+    temp = (node)malloc(sizeof(struct LinkedList));
+    temp->next = NULL;
+    return temp;
+}
+
+node addNode(node head, char *new_str) {
+    char *dest;  // Create new str
+    dest = malloc(sizeof(char) * (strlen(new_str) + 1));
+    strcpy(dest, new_str);  // Copy string into a new string
+
+    node temp, p;
+    temp = createNode();
+
+    temp->str = dest;  // Pointer to new string
+    if (head == NULL) {
+        head = temp;
+    } else {
+        p = head;
+        while (p->next != NULL) {
+            p = p->next;
+        }
+        p->next = temp;
+    }
+    return head;
+}
+
+// https://stackoverflow.com/a/230070/7924557
+int file_exists(char *filename) {
+    struct stat buffer;
+    return (stat(filename, &buffer) == 0);
+}
+
+// https://stackoverflow.com/a/24544128/7924557
+int is_folder(char *folder) {
+    struct stat sb;
+    return (stat(folder, &sb) == 0 && S_ISDIR(sb.st_mode));
+}
+
+node listFiles(char *path) {
+    node files_list = createNode();  // Tail of the list
+    node head = files_list;
+
+    FILE *fp;
+    fp = popen("find . -type f", "r");  // Open pointer to command output
+    if (fp == NULL) {
+        printf("Failed to run command");
+        return files_list;
+    }
+
+    char p[1000];  // Max lenght of the path
+    while (fgets(p, sizeof(p), fp) != NULL) {
+        head = addNode(head, p);
+    }
+
+    pclose(fp);               // Close pointer to command output
+    return files_list->next;  // First node is NULL, so return second
+}
+
+int main() {
+    // TODO: check priviledges
+    int res;
+    char *path = "run.sh";
+    if (file_exists(path))
+        if (is_folder(path)){
+            node file_list = listFiles(path);
+            while (file_list != NULL) {
+                printf("%s", file_list->str); // Just for demonstration
+                file_list = file_list->next;
+            }
+        } else {
+            printf("is a file\n");
+        }
+    else
+        printf("no file nor folder found\n");
+
+    return 0;
 }
